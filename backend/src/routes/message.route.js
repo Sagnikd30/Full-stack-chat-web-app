a787ff8ec710e64229f7d97c3630906506bffc92
Sagnikd30 <sagnikd30@gmail.com>
2025-06-06 13:21:01 +0530

    Fix typo in getUsersForSidebar import

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js";
-import { gestUsersForSidebar, sendMessage, getMessages } from "../controllers/message.controller.js";
+import { getUsersForSidebar, sendMessage, getMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users", protectRoute, gestUsersForSidebar);
+router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id",protectRoute,getMessages);
 router.post("/send/:id",protectRoute,sendMessage);
 
