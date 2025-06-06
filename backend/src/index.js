13cbe52c324a326936911374581394663c1caca1
Sagnikd30 <sagnikd30@gmail.com>
2025-06-06 12:56:07 +0530

    Fix __dirname issue in ES module for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,37 +2,42 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import {connectDB} from "./lib/db.js";
-
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import {app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
-dotenv.config()
+dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT;
-const _dirname = path.resolve();
 
-app.use(express.json({limit: "25mb"}));
+app.use(express.json({ limit: "25mb" }));
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Or your frontend Render domain
     credentials: true,
-}))
+  })
+);
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(_dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
-    app.get("*",(req,res) => {
-        res.sendFile(path.join(_dirname, "../frontend","dist","index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
 }
-server.listen(PORT, () =>{
-    console.log("server is running on port:"+ PORT);
-    connectDB()
+
+server.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+  connectDB();
 });
